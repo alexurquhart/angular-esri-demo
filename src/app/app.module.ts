afab9b686e5e6b31f8c1421faa40364ada6cc97c
cdn-madness <alexurquhart@gmail.com>
2017-10-13 19:12:48 -0400

    formatted front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './modules/material/material.module';
+import { ArcgisModule } from './modules/arcgis/arcgis.module';
 
 import { AppComponent } from './app.component';
-import { MaterialModule } from './modules/material/material.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    ArcgisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
